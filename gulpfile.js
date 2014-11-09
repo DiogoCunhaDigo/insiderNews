fe61c5845f143f5ef56e252dedf5afd133c22535
Filipe Deschamps <filiped@gmail.com>
2014-11-09 12:49:23 +0000

    Refatoracao dos testes

--- gulpfile.js
+++ gulpfile.js
@@ -4,25 +4,28 @@ var mocha = require('gulp-mocha');
 var gulpUtil = require('gulp-util');
 var liveReload = require('gulp-livereload');
 
-// Módulos utilizados nos testes.
-// Na configuração da task 'run-mocha' eles
-// serão injetados no mocha de forma global.
-var chai = require('chai');
-var chaiAsPromised = require('chai-as-promised');
-var should = chai.should();
-chai.use(chaiAsPromised);
 
 gulp.task('run-mocha', function() {
+  
+  // Módulos utilizados que serão injetados
+  // de forma global no mocha.
+  var chai = require('chai');
+  var chaiAsPromised = require('chai-as-promised');
+  var should = chai.should();
+  var expect = chai.expect;
+  chai.use(chaiAsPromised);
+  
   return gulp.src(['test/*.js'], { read: false })
     .pipe(mocha({
       reporter: 'list',
       globals: {
         chai: chai,
-        should: should
+        should: should,
+        expect: expect
       }
     }))
 });
 
 gulp.task('develop-test', function() {
-  gulp.watch(['test/**', 'content/configurations.js', 'core/**'], ['run-mocha']);
+  gulp.watch(['test/**', 'gulpfile.js', 'content/configurations.js', 'core/**'], ['run-mocha']);
 });
--- test/web-server.js
+++ test/web-server.js
@@ -1,33 +1,41 @@
-var configurations = require('../core/configurations');
+var configurations = require('../core/configurations/index.js');
 var createWebServer = require(configurations.paths.server + 'web-server/index.js');
 
 describe('createWebServer', function() {
   
-  it('#start() should start with default configurations', function() {
-    var webServer = createWebServer();
-    var startPromise = webServer.start();
+  describe('#start()', function() {
+    it('should start with default configurations', function() {
+      var webServer = createWebServer();
+      var startPromise = webServer.start();
+      
+      startPromise
+        .then(function() {
+          webServer.stop();
+        })
+        .catch(function() {
+          webServer.stop();
+        })
+  
+        return startPromise.should.be.fulfilled;
+    })
     
-    startPromise
-      .then(function(server) {
-        server.host.should.equal('0.0.0.0');
-        webServer.stop();
-      })
-      .catch(function() {
-        webServer.stop();
-      })
-
-      return startPromise.should.be.fulfilled;
   })
   
-  it('#stop() should stop the web server', function() {
-    var webServer = createWebServer();
+  
+  describe('#stop()', function() {
     
-    return webServer
-      .start()
-      .then(function() {
-        return webServer.stop().should.be.fulfilled;
-      })
+    it('should stop the web server', function() {
+      var webServer = createWebServer();
+      var stopPromise;
+      
+      return webServer
+        .start()
+        .then(function() {
+          stopPromise = webServer.stop();
+          return stopPromise.should.be.fulfilled;
+        })
+    })
     
   })
+  
 })
-

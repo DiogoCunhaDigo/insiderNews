3cf0c6dd3b2432b25ed914dabecc608fd718cc4f
Filipe Deschamps <filiped@gmail.com>
2014-11-09 13:18:57 +0000

    Adiciona teste para verificar retorno da promise do webServer

--- test/web-server.js
+++ test/web-server.js
@@ -12,11 +12,24 @@ describe('createWebServer', function() {
         .then(function() {
           webServer.stop();
         })
-        .catch(function() {
+  
+        return startPromise.should.be.fulfilled;
+    })
+    
+    it('should return default values when no one specified', function() {
+      var webServer = createWebServer();
+      var startPromise = webServer.start();
+      var defaultPromiseReturn = {
+        host: configurations.defaults.webServerHost,
+        port: configurations.defaults.webServerPort
+      }
+      
+      startPromise
+        .then(function() {
           webServer.stop();
         })
   
-        return startPromise.should.be.fulfilled;
+        return startPromise.should.eventually.deep.equal(defaultPromiseReturn);
     })
     
   })

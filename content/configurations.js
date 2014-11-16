9deb99a306bb1e01dbc11a98acfe2e96a65b450f
Filipe Deschamps <filiped@gmail.com>
2014-11-16 16:46:47 -0200

    Move a lógica de configuração de host e port para as configurações do core

--- content/configurations.js
+++ content/configurations.js
@@ -1,14 +1,14 @@
 var configurations = {};
 
 // CONFIGURAÇÕES DO USUÁRIO
-// Altere os valores abaixo para configurar o sistema como desejar.
+// Personalize o insiderNews alterando os valores abaixo:
 
 	configurations.name = "insiderNews 2.0 Alpha";
 	
 	// SERVIDOR WEB
 	// Configure abaixo o Host e Porta desejada
-	configurations.webServerHost = process.env.INSIDER_HOST || process.env.HOST || '0.0.0.0';
-	configurations.webServerPort = process.env.INSIDER_PORT || process.env.PORT || 80;
+	// configurations.webServerHost = '0.0.0.0';
+	// configurations.webServerPort = 80;
 
 // CONFIGURAÇÕES DO USUÁRIO [fim]
 

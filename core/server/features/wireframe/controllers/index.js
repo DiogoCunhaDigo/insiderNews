4d2e338522cc3b68e2a7cb7a7f44fff11f5202b2
Filipe Deschamps <filiped@gmail.com>
2014-11-22 11:38:01 +0000

    Adiciona esqueleto do layout, controller e rota do wireframe. Closes #39

---
+++ core/server/features/wireframe/controllers/index.js
@@ -0,0 +1,9 @@
+var controllers = {};
+
+controllers.mainPage = function(req, res) {
+    res.render('wireframe/views/main-page.html', {
+        title: 'insiderNews - Wireframe'
+    });
+}
+
+module.exports = controllers;

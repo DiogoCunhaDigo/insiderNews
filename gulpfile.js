37046bfaf621d26070fd66d793fe59871e658d99
Filipe Deschamps <filiped@gmail.com>
2014-12-14 15:03:08 -0200

    Adiciona o ng-anotate

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,8 @@ var watchify = require('watchify');
 var source = require('vinyl-source-stream');
 var plumber = require('gulp-plumber');
 var gulpif = require('gulp-if');
+var ngAnnotate = require('gulp-ng-annotate');
+var streamify = require('gulp-streamify');
 var taskName = gutil.env._[0];
 
 var errorHandler = function(error) {
@@ -95,6 +97,7 @@ gulp.task('build-scripts', function() {
   .bundle()
   .pipe(plumber())
   .pipe(source('site.js'))
+  .pipe(streamify(ngAnnotate()))
   .pipe(gulp.dest(configurations.paths.content.themes + 'default/scripts/'));
 });
 

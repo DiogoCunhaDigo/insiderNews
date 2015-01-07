c35817ae1f97cf91018e529a53b8bbe0558ee761
Filipe Deschamps <filiped@gmail.com>
2015-01-07 21:45:29 -0200

    Adiciona a pasta controllers

---
+++ core/client/site/news-feed/controllers/news-feed.controller.js
@@ -0,0 +1,34 @@
+'use strict';
+
+angular.module('in.newsFeed').controller('NewsFeedCtrl', NewsFeedCtrl);
+
+function NewsFeedCtrl() {
+  var newsFeed = this;
+
+  newsFeed.news = [
+    {
+      title: "Warren Buffett faz doação para apoiar Hillary Clinton",
+      lastComment: "comiranda: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      xp: 250,
+      slug: "warren-buffett-faz-doacao-para-apoiar-hillary-clinton"
+    },
+    {
+      title: "Semana negativa? Queda sutil do Ibovespa aumenta as chances de rali de alta - InfoMoney",
+      lastComment: "guerreiro: Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      xp: 145,
+      slug: "semana-negativa-queda-sutil-do-ibovespa-aumenta-as-chances-de-rali-de-alta-infomoney"
+    },
+    {
+      title: "Brasil cresce só 0,1% com gastos públicos",
+      lastComment: "rebatti: Duis aute irure dolor in reprehenderit in voluptate velit.",
+      xp: 130,
+      slug: "brasil-cresce-so-01-com-gastos-publicos"
+    },
+    {
+      title: "Black Friday nos EUA tem filas e muitos brasileiros",
+      lastComment: "hf: Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      xp: 100,
+      slug: "black-friday-nos-eua-tem-filas-e-muitos-brasileiros"
+    }
+  ];
+}

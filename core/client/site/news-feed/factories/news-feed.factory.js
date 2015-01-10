5e822b43ab16cfd9baab8a16a2731f4fa3681949
Filipe Deschamps <filiped@gmail.com>
2015-01-10 00:56:16 -0200

    Factory agora utiliza o Firebase para dados

--- core/client/site/news-feed/factories/news-feed.factory.js
+++ core/client/site/news-feed/factories/news-feed.factory.js
@@ -1,41 +1,22 @@
 'use strict';
 
-angular.module('in.newsFeed').factory('createNewsFeed', function createNewsFeedFactory($q) {
+angular.module('in.newsFeed').factory('createNewsFeed', function createNewsFeedFactory($q, $http) {
 
-  function createNewsFeed() {
+  function createNewsFeed(spec) {
+    spec = spec || {};
+    var repository = spec.repository || $http;
 
-    var newsList = [
-      {
-        title: "Black Friday nos EUA tem filas e muitos brasileiros",
-        lastComment: "hf: Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        xp: 100,
-        slug: "black-friday-nos-eua-tem-filas-e-muitos-brasileiros"
-      },
-      {
-        title: "Semana negativa? Queda sutil do Ibovespa aumenta as chances de rali de alta - InfoMoney",
-        lastComment: "guerreiro: Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        xp: 145,
-        slug: "semana-negativa-queda-sutil-do-ibovespa-aumenta-as-chances-de-rali-de-alta-infomoney"
-      },
-      {
-        title: "Warren Buffett faz doação para apoiar Hillary Clinton",
-        lastComment: "comiranda: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        xp: 250,
-        slug: "warren-buffett-faz-doacao-para-apoiar-hillary-clinton"
-      },
-      {
-        title: "Brasil cresce só 0,1% com gastos públicos",
-        lastComment: "rebatti: Duis aute irure dolor in reprehenderit in voluptate velit.",
-        xp: 130,
-        slug: "brasil-cresce-so-01-com-gastos-publicos"
-      }
-    ];
+    var newsList = [];
 
     function find() {
 
       /*jshint newcap: false */
       return new $q(function findNews(resolve, reject) {
-        resolve(newsList);
+
+        repository.get('https://dazzling-heat-7137.firebaseio.com/news.json')
+          .success(function(data) {
+            resolve(data);
+          });
       });
     }
 

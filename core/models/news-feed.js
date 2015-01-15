31012de13fe1781ead24cc7780ffd6b075d33f08
Filipe Deschamps <filiped@gmail.com>
2015-01-14 22:32:32 -0200

    Faz o model newsFeed ter interface de event emitter

--- core/models/news-feed.js
+++ core/models/news-feed.js
@@ -1,32 +1,55 @@
 'use strict';
+var EventEmitter = require('events').EventEmitter;
 
 function createNewsFeed(spec) {
   spec = spec || {};
   var repository = spec.repository || undefined;
+  var events = new EventEmitter();
+  var newsList = [];
 
   if ( !repository ) {
     throw new Error('You need to specify the repository property: "createNewsFeed({repository: repositoryObject})"');
   }
 
   function find() {
-
     return new Promise(function findNews(resolve, reject) {
 
       repository
         .find()
-        .then(function success(news) {
-          resolve(news);
+        .then(function success(newsList) {
+          events.emit('newsList:update', newsList);
+          resolve(newsList);
         })
         .catch(function error(result) {
           reject(result);
         });
 
     });
+  }
+
+  function start() {
+
+    /* ONLY MOCKING */
+    var sum = 0;
+
+    setInterval(function() {
+      sum = sum + 1;
+      newsList.push({
+        "lastComment": "Comentário: " + sum,
+        "slug": sum,
+        "title": "Título " + sum,
+        "xp": sum
+      });
+
+      events.emit('newsList:updated', newsList);
+    }, 50);
 
   }
 
-  return Object.freeze({
-    find: find
+  return Object.create({
+    find: find,
+    start: start,
+    events: events
   });
 
 }

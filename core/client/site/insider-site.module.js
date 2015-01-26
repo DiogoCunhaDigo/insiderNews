df57cd9703ad3a3997eb64df3115b19c91685398
Filipe Deschamps <filiped@gmail.com>
2015-01-26 20:56:14 -0200

    Refatora a declaração dos módulos no insider-site

--- core/client/site/insider-site.module.js
+++ core/client/site/insider-site.module.js
@@ -4,11 +4,17 @@ require('native-promise-only');
 require('angular');
 require('angular-animate');
 require('angular-ui-router');
+
 require('./news/news.module.js');
+require('./news-feed/news-feed.module.js');
+require('./news-detail/news-detail.module.js');
+
 
 angular.module('in.site', [
   'in.templates',
   'in.news',
+  'in.newsFeed',
+  'in.newsDetail',
   'ui.router',
   'ngAnimate'
 ]);

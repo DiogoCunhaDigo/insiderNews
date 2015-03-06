4eec7cbc21c429cc8614d8efcf2436bd5d07c0b0
Filipe Deschamps <filiped@gmail.com>
2015-03-05 22:42:35 -0300

    Começa a modelagem do model User e News do server-side

--- core/server/models/news.js
+++ core/server/models/news.js
@@ -5,6 +5,11 @@ var database = require('../database/index.js');
 var sequelize = database.sequelize;
 
 var News = sequelize.define('news', {
+  id: {
+    type: Sequelize.UUID,
+    defaultValue: Sequelize.UUIDV1,
+    primaryKey: true
+  },
   title: {
     type: Sequelize.STRING
   }
